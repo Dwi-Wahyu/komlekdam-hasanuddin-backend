6fd9c196fb53eef184982dee0298c8c3559abd40
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2025-07-28 10:14:15 +0800

    fix create last migration

---
+++ prisma/migrations/20250728021337_fix_deskripsi_length/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE `kegiatan` MODIFY `deskripsi` TEXT NOT NULL,
+    MODIFY `detail` LONGTEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE `litbang` MODIFY `deskripsi` TEXT NOT NULL,
+    MODIFY `detail` LONGTEXT NOT NULL;
